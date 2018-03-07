1695b7c13191e1656f85a4fd304a677fbb590934
Kyle Patterson <kdpatters@gmail.com>
2018-03-07 00:51:12 -0500

    Changed comments

--- drug_simulation.m
+++ drug_simulation.m
@@ -15,7 +15,7 @@ rate_constant = log(2) / halflife_drug; % Exponential decay: 1/2 = e^{-kt}
 hours = 75;
 % interval MUST be an integer of function will break
 interval = 24; % number of hours between when each pill is taken
-num_points = 25; % how many points to graph, including t = 0
+num_points = 25; % how many points to graph, not including t = 0
 
 % Intermediate calculations
 sub_divisions = floor((num_points) / (hours/interval));
